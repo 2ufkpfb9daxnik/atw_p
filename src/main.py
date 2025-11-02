ec666db0d3908b4e0efa8ba69e1af76f3e735fd9
9k3pthhurh8f9z <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2025-11-03 08:35:04 +0900

    フォントサイズが変更できるようになった。しかしフォントサイズをまた小さくすると、レイアウトが崩れたままになる。

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ from PyQt6.QtGui import QFontDatabase, QFont
 import os
 
 class SettingsWindow(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, app: QApplication, loaded_family: str | None, parent=None):
         super().__init__(parent)
+        self.app = app
+        self.loaded_family = loaded_family
+
         self.setWindowTitle("設定")
         layout = QVBoxLayout()
 
@@ -13,15 +16,18 @@ class SettingsWindow(QWidget):
         self.label = QLabel("現在のフォントサイズ")
         layout.addWidget(self.label)
         # 現在のフォントサイズ表示
-        self.current_size = QLabel("10")
+        self.current_size = QLabel(str(self._current_font_size))
         layout.addWidget(self.current_size)
+
         # スピンボックス
         self.spin = QSpinBox()
         self.spin.setRange(8, 255)
-        self.spin.setValue(10)
+        self.spin.setValue(self._current_font_size())
         layout.addWidget(self.spin)
+        
         # フォントサイズを変更するのを決定するボタン
         apply_button = QPushButton("適用")
+        apply_button.clicked.connect(self.on_apply)
         layout.addWidget(apply_button)
 
         # 設定ウィンドウを閉じるボタン
@@ -29,6 +35,23 @@ class SettingsWindow(QWidget):
         close_button.clicked.connect(self.close)
         layout.addWidget(close_button)
         self.setLayout(layout)
+
+    def _current_font_size(self) -> int:
+        pointSize = self.app.font().pointSize()
+        if pointSize == -1:
+            pointSize = 10
+        return pointSize
+    
+    def on_apply(self):
+        new_size = int(self.spin.value())
+        # 現在のフォントサイズのラベル更新
+        self.current_size.setText(str(new_size))
+        # アプリ全体のフォントを更新
+        if self.loaded_family:
+            font = QFont(self.loaded_family, new_size)
+        else:
+            font = QFont("", new_size)
+        self.app.setFont(font)
 
 def main():
     app = QApplication([])
@@ -57,7 +80,8 @@ def main():
     layout.addWidget(exit_button)
 
     # 設定ウィンドウ表示ボタンの動作
-    settings_window = SettingsWindow()
+    loaded_family = app.font().family()
+    settings_window = SettingsWindow(app, loaded_family)
     def open_settings():
         settings_window.show()
         settings_window.raise_()
