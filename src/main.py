45341c0b7f8d4ac18ef6f71dee885764742abb54
9k3pthhurh8f9z <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2025-11-03 08:03:23 +0900

    フォントサイズの変更のために、まず見た目だけを作成

--- src/main.py
+++ src/main.py
@@ -1,19 +1,34 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QSpinBox, QLabel
 from PyQt6.QtGui import QFontDatabase, QFont
 import os
-from settings import Settings
 
 class SettingsWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("設定")
         layout = QVBoxLayout()
-        
+
+        # フォントサイズ設定
+        # タイトル
+        self.label = QLabel("現在のフォントサイズ")
+        layout.addWidget(self.label)
+        # 現在のフォントサイズ表示
+        self.current_size = QLabel("10")
+        layout.addWidget(self.current_size)
+        # スピンボックス
+        self.spin = QSpinBox()
+        self.spin.setRange(8, 255)
+        self.spin.setValue(10)
+        layout.addWidget(self.spin)
+        # フォントサイズを変更するのを決定するボタン
+        apply_button = QPushButton("適用")
+        layout.addWidget(apply_button)
+
+        # 設定ウィンドウを閉じるボタン
         close_button = QPushButton("閉じる")
         close_button.clicked.connect(self.close)
         layout.addWidget(close_button)
         self.setLayout(layout)
-
 
 def main():
     app = QApplication([])
@@ -32,14 +47,14 @@ def main():
     window.setWindowTitle("atw")
     layout = QVBoxLayout()
 
+    # 設定ウィンドウ表示ボタン
+    settings_button = QPushButton("設定")
+    layout.addWidget(settings_button)
+
     # プログラム終了ボタン
     exit_button = QPushButton("終了")
     exit_button.clicked.connect(app.quit)
     layout.addWidget(exit_button)
-
-    # 設定ウィンドウ表示ボタン
-    settings_button = QPushButton("設定")
-    layout.addWidget(settings_button)
 
     # 設定ウィンドウ表示ボタンの動作
     settings_window = SettingsWindow()
